dc9433915865a91501006ad006e16bfbb0ba995a
Owen <owencasabuena@gmail.com>
2024-11-12 19:40:51 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, flash, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+''' 
+-----------------------------------------------
+               Routes Section
+-----------------------------------------------
+'''
+
